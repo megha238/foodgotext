d445e0dd41495c14aaeb70721b1429b7dc4c7c7a
Abhishek Yadav <82928998+yabhihsek9575@users.noreply.github.com>
2024-05-24 00:45:01 +0530

    hi jii
    meha jii

--- lib/ChatBloc/chat_bloc.dart
+++ lib/ChatBloc/chat_bloc.dart
@@ -6,13 +6,15 @@ import 'customer_support.dart';
 
 class ChatBloc extends Bloc<ChatEvent,ChatState>{
   List <ChetMessage>tags = [];
+    List <String>defaultList = [];
+
   TextEditingController textController = TextEditingController();
   ChatBloc() : super(ChatInitialState()){
     on((event, emit) {
       if(event is ChatSendBtnEvent){
 
-        tags.add(ChetMessage(message: event.leftMsg,isSender: false));
-        tags.add(ChetMessage(message: event.rightMsg,isSender: true));
+        tags.add(ChetMessage(message: event.leftMsg ?? '',isSender: false));
+        tags.add(ChetMessage(message: event.rightMsg ?? '',isSender: true));
         return emit(ChatAddState(chat: textController.text));
       }
     });
